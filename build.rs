e4c88a909f155149f2c27c67ad4399ff905cb7d2
Jenyyk <jendotron2@gmail.com>
2025-11-21 20:26:49 +0100

    dev: add windows support

--- build.rs
+++ build.rs
@@ -4,21 +4,30 @@ fn main() {
     let manifest_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
 
     let target = std::env::var("TARGET").unwrap();
-    let target_first = target.split("-").next().unwrap();
+    let mut target_iter = target.split("-");
+    let lib_target = match target_iter.next() {
+        Some("aarch64") => "aarch64",
+        Some("x86_64") => match target_iter.next() {
+            Some("unknown") => "x86_64",
+            Some("pc") => "windows",
+            _ => panic!("Unsupported target"),
+        },
+        _ => panic!("Unsupported target"),
+    };
 
-    println!("cargo:warning=using lib {}", target_first);
+    println!("cargo:warning=using lib {}", lib_target);
 
     println!(
         "cargo:rustc-link-search={}",
         manifest_dir
-            .join(format!("lib/{target_first}/"))
+            .join(format!("lib/{lib_target}/"))
             .to_str()
             .unwrap()
     );
     println!("cargo:rustc-link-lib=dylib=pxcore");
 
     // aarch64 target requires libzest.so during compile time
-    if target_first == "aarch64" {
+    if lib_target == "aarch64" {
         println!("cargo:rustc-link-lib=dylib=zest")
     }
 }
